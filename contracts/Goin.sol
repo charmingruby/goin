73cff5c7cb6016b18980744d0e2826c5fb48d737
charmingruby <mrrubycanal3105@gmail.com>
2025-03-30 10:14:36 -0300

    feat: creates transfer and balanceOf impls

--- contracts/Goin.sol
+++ contracts/Goin.sol
@@ -9,4 +9,35 @@ contract Goin {
     string public symbol = "GOIN";
     uint8 public decimals = 18;
     uint256 public totalSupply = 1000 * 10 ** decimals;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) private balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf(msg.sender) >= _value, "Insufficient balance");
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
 }
--- test/Goin.ts
+++ test/Goin.ts
@@ -2,7 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
 
-describe("Goin", function () {
+describe("Goin", () => {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
@@ -15,29 +15,66 @@ describe("Goin", function () {
     return { goin, owner, otherAccount };
   }
 
-  describe("ERC-20 state variables", function () {
-    it("should have the correct name", async function () {
+  describe("ERC-20 state variables", () => {
+    it("should have the correct name", async () => {
       const { goin, owner, otherAccount } = await loadFixture(deployFixture);
 
       expect(await goin.name()).to.equal("Goin");
     });
 
-    it("should have the correct symbol", async function () {
+    it("should have the correct symbol", async () => {
       const { goin, owner, otherAccount } = await loadFixture(deployFixture);
 
       expect(await goin.symbol()).to.equal("GOIN");
     });
 
-    it("should have the correct decimals", async function () {
+    it("should have the correct decimals", async () => {
       const { goin, owner, otherAccount } = await loadFixture(deployFixture);
 
       expect(await goin.decimals()).to.equal(18);
     });
 
-    it("should have the correct totalSupply", async function () {
+    it("should have the correct totalSupply", async () => {
       const { goin, owner, otherAccount } = await loadFixture(deployFixture);
 
       expect(await goin.totalSupply()).to.equal(1000n * 10n ** 18n);
+    });
+  });
+
+  describe("ERC-20 methods", () => {
+    describe("balanceOf", () => {
+      it("should return the correct balance for the owner", async () => {
+        const { goin, owner, otherAccount } = await loadFixture(deployFixture);
+
+        expect(await goin.balanceOf(owner.address)).to.equal(1000n * 10n ** 18n);
+      });
+    });
+
+    describe("transfer", () => {
+      it("should transfer tokens between accounts", async () => {
+        const { goin, owner, otherAccount } = await loadFixture(deployFixture);
+
+        const initialOwnerBalance = await goin.balanceOf(owner.address);
+        const initialOtherAccountBalance = await goin.balanceOf(otherAccount.address);
+
+        await goin.transfer(otherAccount.address, 100n);
+
+        const finalOwnerBalance = await goin.balanceOf(owner.address);
+        const finalOtherAccountBalance = await goin.balanceOf(otherAccount.address);
+
+        expect(initialOwnerBalance).to.equal(1000n * 10n ** 18n);
+        expect(finalOwnerBalance).to.equal(initialOwnerBalance - 100n);
+        expect(initialOtherAccountBalance).to.equal(0);
+        expect(finalOtherAccountBalance).to.equal(100n);
+      });
+
+      it("should be not able to transfer tokens between accounts if the sender does not have enough balance", async () => {
+        const { goin, owner, otherAccount } = await loadFixture(deployFixture);
+
+        const otherAccountInstance = goin.connect(otherAccount);
+
+        await expect(otherAccountInstance.transfer(owner.address, 1n)).to.be.revertedWith("Insufficient balance");
+      });
     });
   });
 });
